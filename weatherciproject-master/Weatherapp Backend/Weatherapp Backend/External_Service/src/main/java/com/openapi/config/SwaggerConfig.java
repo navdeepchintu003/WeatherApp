751ac203bb8e0f3752fa46530ceff864ea7e85a6
KyasagallaChintu <kyasagallachintu@gmail.com>
2022-09-21 20:36:14 +0530

    weather

---
+++ weatherciproject-master/Weatherapp Backend/Weatherapp Backend/External_Service/src/main/java/com/openapi/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.openapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+
+	
+	public class SwaggerConfig {
+	   
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.basePackage("com.openapi"))
+					.build();
+		}
+	
+
+}
+
+
+
